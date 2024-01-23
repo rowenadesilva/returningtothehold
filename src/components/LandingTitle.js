f4ca32c326fc5a4d49effaab722f92fb4e17c5ea
Finnste <Finn.Steffens@gmx.de>
2024-01-23 15:58:52 +0000

    header and first text elements

---
+++ src/components/LandingTitle.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+class LandingTitle extends React.Component {
+  render() {
+    return (
+      <div className="titleWrapper">
+        <div className="landingTitle">Returning to the Hold</div>
+        <div className="landingCopy">
+          <p>
+            Bibby Stockholm is a barge berthed at Portland Harbour on the south
+            coast of England, where it will detain 500 migrants. The history of
+            Bibby Stockholm as a detention site, paired with the company’s
+            longer legacy in the transatlantic slave trade, contextualises
+            contemporary border practices within a larger narrative of
+            colonialism and empire. Indebted to Christina Sharpe and Édouard
+            Glissant, this work demonstrates how Bibby Stockholm sits in the
+            lineage of the architecture and logic of slave ships, looking
+            towards the ocean as an archive of this violence. Through this lens,
+            Bibby Stockholm is an object to enter into the border-migration
+            nexus and legacies of slavery–both intimately tied to the spatial
+            politics of incarceration and border techniques situated
+            within maritime space.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LandingTitle;
