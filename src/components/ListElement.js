df2a9a9b5eee3cf7b72cc37bf35bc0882bb17b90
finndste <finn.steffens@gmx.de>
2024-04-02 00:29:04 +0100

    tracker solved with path text

--- src/components/ListElement.js
+++ src/components/ListElement.js
@@ -3,30 +3,38 @@ import Tracker from "./Tracker";
 import { ReactComponent as SVG } from "../assets/TestLine.svg";
 import styled from "styled-components";
 
-const ListInnerContainer = styled.div`
-  border: 1px solid yellow;
+const Line = styled.svg`
+  stroke: black;
+  stroke-width: 3;
+  fill: none;
+  margin-top: -2px;
+  margin-bottom: -2px;
 `;
 
-const Line = styled(SVG)`
-  border: 1px solid green;
-`;
-
-const ListElement = ({focus}) => {
+const ListElement = ({ focus, trackerPos }) => {
   const svgRef = useRef();
-  const listInnerContainerRef = useRef();
 
   return (
     <div>
-      <ListInnerContainer ref={listInnerContainerRef}>
-        <Tracker focus={focus}/>
-      </ListInnerContainer>
       <Line
         ref={svgRef}
-        svg={SVG}
+        viewBox="0 0 116 1400"
+
         style={{
           background: focus ? "yellow" : "red",
         }}
-      />
+      >
+        <path
+          id="curve"
+          d="M58.33 0C26.12 0 0 26.12 0 58.33s26.12 58.33 58.33 58.33 58.33 26.12 58.33 58.33-26.12 58.33-58.33 58.33S0 259.45 0 291.67 26.12 350 58.33 350s58.33 26.12 58.33 58.33-26.12 58.33-58.33 58.33S0 492.78 0 525s26.12 58.33 58.33 58.33 58.33 26.12 58.33 58.33S90.55 700 58.33 700 0 726.12 0 758.33s26.12 58.33 58.33 58.33 58.33 26.12 58.33 58.33-26.12 58.33-58.33 58.33S0 959.45 0 991.67 26.12 1050 58.33 1050s58.33 26.12 58.33 58.33c0 32.22-26.12 58.33-58.33 58.33S0 1192.78 0 1225s26.12 58.33 58.33 58.33 58.33 26.12 58.33 58.33c0 32.22-26.12 58.33-58.33 58.33"
+        ></path>
+        <text className="text" style={{ fill: "red" }}>
+          <textPath startOffset={trackerPos + "%"} href="#curve">
+          ·
+          </textPath>
+        </text>
+      </Line>
+      {/* <Tracker focus={focus} trackerPos={trackerPos} /> */}
     </div>
   );
 };
--- src/pages/MapScroll.js
+++ src/pages/MapScroll.js
@@ -22,11 +22,12 @@ const LIST = styled.li`
 `;
 
 const App = () => {
+  const focus = [false, true, false];
+
   const listRef = useRef([]);
   const wrapperRef = useRef();
 
-  const [focus, setFocus] = useState([false, true, false]);
-
+  const [trackerPos, setTrackerPos] = useState(0);
   // new list with two elements
   const [list, setList] = useState([
     {
@@ -43,6 +44,7 @@ const App = () => {
   useEffect(() => {
     wrapperRef.current.addEventListener("scroll", function () {
       checkNewElements();
+      updateTrackerPos();
     });
   });
 
@@ -60,12 +62,12 @@ const App = () => {
     const inFocus2 = rect2.top <= wrapperRef.current.clientHeight / 2;
 
     // add new list element if top is reached and delete bottom element
-    if (inFocus0 && list.length == 3) {
+    if (inFocus0 && list.length === 3) {
       addTop();
     }
 
     // add new list element if bottom is reached and delete top element
-    if (inFocus2 && list.length == 3) {
+    if (inFocus2 && list.length === 3) {
       addBottom();
     }
   };
@@ -88,6 +90,15 @@ const App = () => {
     setList(copy);
   };
 
+  const updateTrackerPos = () => {
+    const absolutePos =
+      listRef.current[1].getBoundingClientRect().top * -1 +
+      wrapperRef.current.clientHeight / 2;
+    const relativePos = (absolutePos / listRef.current[1].clientHeight) * 100;
+    console.log(relativePos);
+    setTrackerPos(relativePos);
+  };
+
   return (
     <Container>
       <Wrapper ref={wrapperRef}>
@@ -99,7 +110,7 @@ const App = () => {
               id={item.id}
               key={item.id}
             >
-              <ListElement focus={focus[i]} />
+              <ListElement focus={focus[i]} trackerPos={trackerPos} />
             </LIST>
           );
         })}
